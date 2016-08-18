132b19b818f0d4628beebdba7394a57777031ec5
realcow <realcow86@gmail.com>
2016-08-19 01:53:57 +0900

    Set working directory as executable file containing directory.

--- peregrine-desktop/main.cpp
+++ peregrine-desktop/main.cpp
@@ -1,10 +1,14 @@
 #include "launcherwindow.h"
 #include <QApplication>
 #include <QtWebView/QtWebView>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QDir::setCurrent(QApplication::applicationDirPath());
+
     LauncherWindow w;
     QtWebView::initialize();
     w.show();
