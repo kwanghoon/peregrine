42864fa049c74fd83d388fa4e3cd955584520ff5
realcow <realcow86@gmail.com>
2016-09-15 00:59:33 +0900

    Fix password hash calculation logic.

--- peregrine-server/public/js/registerlib.ts
+++ peregrine-server/public/js/registerlib.ts
@@ -1,30 +1,15 @@
-﻿function hex(buffer: ArrayBuffer): string {
-    let hexCodes: string[] = [];
-    let view = new DataView(buffer);
-    for (let i = 0; i < view.byteLength; i += 4) {
-        // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
-        let value = view.getUint32(i)
-        // toString(16) will give the hex representation of the number without padding
-        let stringValue = value.toString(16)
-        // We use concatenation and slice for padding
-        let padding = '00000000'
-        let paddedValue = (padding + stringValue).slice(-padding.length)
-        hexCodes.push(paddedValue);
-    }
-
-    // Join all the hex strings into one
-    return hexCodes.join("");
-}
-
-function twistPassword(email: string, pw: string): Promise<string> {
-    let salted = 'zelkova' + pw + 'x' + email + '-peregrine';
+﻿function twistPassword(email: string, pw: string): Promise<string> {
+    let salted: string = 'zelkova-' + pw + 'x' + email + '-peregrine';
     let pwBuff = new Uint8Array(salted.length);
     for (let i = 0; i < salted.length; i++) {
-        pwBuff[i] = salted[i].charCodeAt(i);
+        pwBuff[i] = salted.charCodeAt(i);
     }
     return new Promise(function (resolve: any, reject: any) {
         window.crypto.subtle.digest({ name: 'SHA-256' }, pwBuff)
-            .then(function (hash: ArrayBuffer) { resolve(hex(hash)); })
+            .then(function (hash: ArrayBuffer) {
+                let pwHashBase64 = btoa(String.fromCharCode(...new Uint8Array(hash)));
+                resolve(pwHashBase64);
+            })
             .catch(() => { reject(); });
     });
 }
