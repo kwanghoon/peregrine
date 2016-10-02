7b296afc9130f696ac2ed8efaa4f7504510c1acd
realcow <realcow86@gmail.com>
2016-10-03 04:53:26 +0900

    Ensure the specified command exists

--- plugins/manview/manview.cpp
+++ plugins/manview/manview.cpp
@@ -13,13 +13,31 @@ int InitializePlugin(const struct PG_FUNC_TABLE* funcTable, const PG_PLUGIN_CONF
 
 int RunAction(const char* actionId, const char* data)
 {
+    QProcess proc;
     char command[512];
+
 #   ifdef Q_OS_WIN
+    sprintf(command, "where %s", data);
+    proc.start(command);
+    if (proc.waitForFinished())
+    {
+        if (proc.exitCode() != 0)
+        {
+            g_funcTable->fpInvokeQmlFunc("setManText", 
+                QString("Cannot find command(%1).").arg(data).toStdString().c_str());
+            return PG_BEHAVIOR_ON_RETURN::PG_REMAIN;
+        }
+    }
+    else
+    {
+        g_funcTable->fpInvokeQmlFunc("setManText", "");
+        return PG_BEHAVIOR_ON_RETURN::PG_REMAIN;
+    }
+
     sprintf(command, "%s /?", data);
 #   else
     sprintf(command, "%s --help", data);
 #   endif
-    QProcess proc;
     proc.start(command);
     QString errorstr = proc.errorString();
     if (proc.waitForFinished())
